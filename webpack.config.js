9aa34ee0b7f12ba5b04708461a7f24ebe36eeb38
Dony Wibowo <1115854+ashcoft@users.noreply.github.com>
2026-04-30 18:22:11 +0700

    Refactor webpack configuration for better readability

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,138 @@
-const path = require('path');const VueLoaderPlugin = require('vue-loader/lib/plugin');module.exports = {entry:'./src/main.js',output:{path:path.resolve(__dirname,'dist'),filename:'cad-viewer.js',library:'CadViewer',libraryTarget:'umd'},module:{rules:[{test:/\.vue$/,loader:'vue-loader'},{test:/\.js$/,loader:'babel-loader',exclude:/node_modules/},{test:/\.css$/,use:['style-loader','css-loader']},{test:/\.(png|jpg|gif|svg)$/,type:'asset/resource'}]},plugins:[new VueLoaderPlugin()],resolve:{extensions:['.js','.vue','.json'],alias:{vue$:'vue/dist/vue.esm.js'}}}
+const path = require('path');
+
+const { VueLoaderPlugin } = require('vue-loader');
+
+
+
+module.exports = {
+  
+  entry: './src/main.js',
+  
+  output: {
+    
+    path: path.resolve(__dirname, 'js'),
+    
+    filename: 'cad-viewer.js',
+    
+    library: 'CadViewer',
+    
+    libraryTarget: 'umd',
+    
+    clean: true
+      
+  },
+  
+  module: {
+    
+    rules: [
+      
+      {
+        
+        test: /\.vue$/,
+        
+        loader: 'vue-loader'
+          
+      },
+      
+      {
+        
+        test: /\.js$/,
+        
+        loader: 'babel-loader',
+        
+        exclude: /node_modules/
+          
+      },
+      
+      {
+        
+        test: /\.css$/,
+        
+        use: ['style-loader', 'css-loader']
+          
+      },
+      
+      {
+        
+        test: /\.(png|jpg|gif|svg)$/,
+        
+        type: 'asset/resource'
+          
+      }
+      
+    ]
+      
+  },
+  
+  plugins: [
+    
+    new VueLoaderPlugin()
+    
+  ],
+  
+  resolve: {
+    
+    extensions: ['.js', '.vue', '.json'],
+    
+    alias: {
+      
+      'vue$': 'vue/dist/vue.esm-bundler.js'
+        
+    }
+      
+  },
+  
+  externals: {
+    
+    '@nextcloud/axios': 'OCA.Core.Axios',
+    
+    '@nextcloud/router': 'OCA.Core.Router',
+    
+    '@nextcloud/l10n': 'OCA.Core.L10n'
+      
+  }
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
