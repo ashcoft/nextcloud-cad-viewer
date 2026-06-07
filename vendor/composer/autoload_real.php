84903f5a40c8699afa3d4b26402c32646411f152
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-06-07 15:53:12 +0000

    refactor: Apply rector changes
    Signed-off-by: GitHub <noreply@github.com>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitf43b0272898fdde9e85ead37b034f301
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitf43b0272898fdde9e85ead37b034f301::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
