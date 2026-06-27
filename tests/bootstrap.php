29323813eb15db7b7e5c74af7a907fa2727d7a4c
Dony Wibowo <1115854+ashcoft@users.noreply.github.com>
2026-06-27 14:59:44 +0700

    Update tests/bootstrap.php
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $serverBootstrap = __DIR__ . '/../../../tests/bootstrap.php';
 if (file_exists($serverBootstrap)) {
-    require_once $serverBootstrap;
+    include_once $serverBootstrap;
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 
