ad7695977a46d61663ebbde66f9952a4799e733c
Dony Wibowo <1115854+ashcoft@users.noreply.github.com>
2026-06-27 15:00:57 +0700

    Update tests/bootstrap.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 $serverBootstrap = __DIR__ . '/../../../tests/bootstrap.php';
-if (file_exists($serverBootstrap)) {
-    include_once $serverBootstrap;
+if (!file_exists($serverBootstrap)) {
+    throw new RuntimeException('Expected Nextcloud test bootstrap at ' . $serverBootstrap);
 }
+require_once $serverBootstrap;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 \OC_App::loadApp(OCA\CadViewer\AppInfo\Application::APP_ID);
